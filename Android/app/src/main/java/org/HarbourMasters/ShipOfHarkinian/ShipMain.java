6524dd004f9ccdbc07f6b196c98feece0e817972
Arthur Helfstein Fragoso <arthur@life.net.br>
2022-07-24 12:09:16 -0300

    Inicial setup for an Android port
    From SDL guide
    https://github.com/libsdl-org/SDL/blob/main/docs/README-android.md

---
+++ Android/app/src/main/java/org/HarbourMasters/ShipOfHarkinian/ShipMain.java
@@ -0,0 +1,10 @@
+
+package org.HarbourMasters.ShipOfHarkinian;
+
+import org.libsdl.app.SDLActivity;
+
+/**
+ * A sample wrapper class that just calls SDLActivity
+ */
+
+public class ShipMain extends SDLActivity { }
